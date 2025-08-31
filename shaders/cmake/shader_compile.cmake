47b49d924ec01f4bbece123a983c52c1b26b9091
Misaka <1150235785@qq.com>
2025-08-31 23:53:09 +0800

    feature: Make the shader compile more reliable.

--- shaders/cmake/shader_compile.cmake
+++ shaders/cmake/shader_compile.cmake
@@ -1,12 +1,16 @@
 function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR DXC_BIN)
     set(working_dir "${SHADER_ROOT_DIR}")
 
-    set(ALL_GENERATED_DXIL_FILES "")
+    set(ALL_GENERATED_BIN_FILES "")
     set(ALL_GENERATED_CPP_FILES "")
 
     if(UNIX)
         execute_process(COMMAND chmod a+x ${DXC_BIN})
     endif()
+
+    set(SHADER_OPTIMIZATION_LEVEL 
+        "$<$<CONFIG:Debug>:Od>$<$<CONFIG:Release>:O3>$<$<CONFIG:RelWithDebInfo>:O2>$<$<CONFIG:MinSizeRel>:Os>"
+    )
 
     foreach(SHADER ${SHADERS})
         # Prepare a header name and a global variable for this shader
@@ -16,46 +20,42 @@ function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR 
         string(REPLACE "." "_" HEADER_NAME ${SHADER_NAME})
         string(TOUPPER ${HEADER_NAME} GLOBAL_SHADER_VAR)
 
-        set(DXIL_FILE "${SHADER_ROOT_DIR}/${GENERATED_DIR}/dxil/${SHADER_NAME}.dxil")
+        set(BIN_FILE "${SHADER_ROOT_DIR}/${GENERATED_DIR}/bin/${SHADER_NAME}.bin")
         set(CPP_FILE "${SHADER_ROOT_DIR}/${GENERATED_DIR}/cpp/${HEADER_NAME}.h")
 
+        set(SHADER_MODEL_TYPE "")
+
         if(SHADER_EXT STREQUAL ".vert")
-            add_custom_command(
-                OUTPUT ${DXIL_FILE}
-                COMMAND ${DXC_BIN} -T vs_6_0 -Od -WX -Zi -E main ${SHADER} -I ${SHADER_INCLUDE_FOLDER} -Fo ${DXIL_FILE}
-                DEPENDS ${SHADER}
-                WORKING_DIRECTORY "${working_dir}"
-            )
+            set(SHADER_MODEL_TYPE "vs_6_0")
         elseif(SHADER_EXT STREQUAL ".frag")
-            add_custom_command(
-                OUTPUT ${DXIL_FILE}
-                COMMAND ${DXC_BIN} -T ps_6_0 -Od -WX -Zi -E main ${SHADER} -I ${SHADER_INCLUDE_FOLDER} -Fo ${DXIL_FILE}
-                DEPENDS ${SHADER}
-                WORKING_DIRECTORY "${working_dir}"
-            )
+            set(SHADER_MODEL_TYPE "ps_6_0")
         elseif(SHADER_EXT STREQUAL ".comp")
-            add_custom_command(
-                OUTPUT ${DXIL_FILE}
-                COMMAND ${DXC_BIN} -T cs_6_0 -Od -WX -Zi -E main ${SHADER} -I ${SHADER_INCLUDE_FOLDER} -Fo ${DXIL_FILE}
-                DEPENDS ${SHADER}
-                WORKING_DIRECTORY "${working_dir}"
-            )
+            set(SHADER_MODEL_TYPE "cs_6_0")
         endif()
 
-        list(APPEND ALL_GENERATED_DXIL_FILES ${DXIL_FILE})
+        if (NOT SHADER_MODEL_TYPE STREQUAL "")
+            add_custom_command(
+                OUTPUT ${BIN_FILE}
+                COMMAND ${DXC_BIN} -T ${SHADER_MODEL_TYPE} -${SHADER_OPTIMIZATION_LEVEL} -E main ${SHADER} -I ${SHADER_INCLUDE_FOLDER} -Fo ${BIN_FILE}
+                DEPENDS ${SHADER}
+                WORKING_DIRECTORY "${working_dir}"
+            )
 
-        add_custom_command(
-            OUTPUT ${CPP_FILE}
-            COMMAND ${CMAKE_COMMAND} -DPATH=${DXIL_FILE} -DHEADER="${CPP_FILE}"
-            -DGLOBAL="${GLOBAL_SHADER_VAR}" -P "${SHADER_ROOT_DIR}/cmake/generate_shader_cpp_file.cmake"
-            DEPENDS ${DXIL_FILE}
-            WORKING_DIRECTORY "${working_dir}"
-        )
+            list(APPEND ALL_GENERATED_BIN_FILES ${BIN_FILE})
 
-        list(APPEND ALL_GENERATED_CPP_FILES ${CPP_FILE})
+            add_custom_command(
+                OUTPUT ${CPP_FILE}
+                COMMAND ${CMAKE_COMMAND} -DPATH=${BIN_FILE} -DHEADER="${CPP_FILE}"
+                -DGLOBAL="${GLOBAL_SHADER_VAR}" -P "${SHADER_ROOT_DIR}/cmake/generate_shader_cpp_file.cmake"
+                DEPENDS ${BIN_FILE}
+                WORKING_DIRECTORY "${working_dir}"
+            )
+
+            list(APPEND ALL_GENERATED_CPP_FILES ${CPP_FILE})
+        endif()
     endforeach()
 
     add_custom_target(${TARGET_NAME}
-        DEPENDS ${ALL_GENERATED_DXIL_FILES} ${ALL_GENERATED_CPP_FILES} SOURCES ${SHADERS}
+        DEPENDS ${ALL_GENERATED_BIN_FILES} ${ALL_GENERATED_CPP_FILES} SOURCES ${SHADERS}
     )
 endfunction()
